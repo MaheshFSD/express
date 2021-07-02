97049070616aa3fa00000932ad9d8160f10d1bcd
MaheshFSD <maheshfsdeveloper@gmail.com>
2021-07-03 01:20:36 +0530

    express post

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const express = require("express")
 const app = express()
 
 app.get('/',(req,res)=>{
-    console.log("Hello Express")
+    console.log("Hello Express- it is from GET Method.")
 })
 
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    console.log("express post request")
+})
+app.delete('/',(req,res)=>[
+
+])
 app.listen(8001,()=>{
     console.log("server listening on 8001")
 })
